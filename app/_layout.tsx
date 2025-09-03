b6c06076682e60f24495afdd0d752338dfe36fe8
boyfromthewell <tnsdyd10@naver.com>
2025-09-03 13:39:58 +0900

    feat: 배포 파일 설정, 푸쉬 알림 기능 위한 보일러플레이트 코드

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,9 +4,19 @@ import { useReactQueryDevTools } from "@dev-plugins/react-query";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { useFonts } from "expo-font";
+import * as Notifications from "expo-notifications";
 import { Stack } from "expo-router";
 import { useEffect } from "react";
 import Toast from "react-native-toast-message";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
 
 export default function RootLayout() {
   const [loaded] = useFonts({
